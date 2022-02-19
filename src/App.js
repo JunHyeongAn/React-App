7ebfdd72915e362c993e95ae6cb07d6a0439eba1
JunHyeongAn <sure083080@gmail.com>
2022-02-20 05:18:06 +0900

    React App Practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(prev => prev + 1);
+  console.log("render");
+  
   return (
-    <div className="App">
-      <h1 className={styles.title}>Welecom back!!</h1>
-      <Button text={"Continue"}/>
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me!!</button>
     </div>
   );
 }
