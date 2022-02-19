838d870cf2f67a5181fa3b22d0c2773730b5f01a
JunHyeongAn <sure083080@gmail.com>
2022-02-20 05:23:31 +0900

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter(prev => prev + 1);
-  console.log("render");
+
   
+  console.log("i run all the time");
+  useEffect(() => 
+    console.log("CALL THE API..."), 
+    []
+  );
+
   return (
     <div>
       <h1>{counter}</h1>
