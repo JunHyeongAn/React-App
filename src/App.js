5dd50bced3e95801b0a69440e9896cd0d8303aea
JunHyeongAn <sure083080@gmail.com>
2022-02-20 05:34:26 +0900

    userEffect practice

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter(prev => prev + 1);
+  const [keyword, setKeyword] = useState("");
 
-  
+  const onClick = () => setCounter(prev => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("i run all the time");
   useEffect(() => 
     console.log("CALL THE API..."), 
     []
   );
-
+  useEffect(() => {
+    if(keyword != "" && keyword.length >= 5)
+      console.log("SEARCH FOR ", keyword);
+  }, [keyword]);
   return (
     <div>
+      <input 
+        value={keyword}
+        type="text" 
+        placeholder="Search here..." 
+        onChange={onChange}
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me!!</button>
     </div>
